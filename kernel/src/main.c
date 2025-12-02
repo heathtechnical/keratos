a0d08e2dc960c82451a5d71ec27bbf30bd412594
Dan Heath <dan@heathtechnical.com>
2025-12-02 22:21:37 +0000

    Removed bitmap references from main.c

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -6,7 +6,6 @@
 #include <debug/qemu.h>
 #include <bootinfo/bootinfo.h>
 #include <string.h>
-#include <bitmap.h>
 #include <types.h>
 #include <mm/earlyalloc.h>
 
@@ -17,25 +16,6 @@ static void hcf(void)
     {
         asm("hlt");
     }
-}
-
-void print_mmap_entry(uint8_t type, uint64_t base, uint64_t length)
-{
-    if (type != BOOTINFO_MEMMAP_USABLE)
-        return;
-
-    kprintf("[init] Memory Region - Base: %p, Length: %d bytes\n", base, length);
-
-    size_t size = length / 4096;
-
-    // Initialise each region's page bitmap
-    bitmap_zero((uint64_t *)base, size);
-
-    // Allocate bitmap pages for bitmap
-    bitmap_set((uint64_t *)base, size, 0, size / BITMAP_WORD_SIZE / 8 + 1);
-
-    ssize_t first_free = bitmap_find_first_zero((const uint64_t *)base, size);
-    kprintf("[init]   First free page at index: %d\n", first_free);
 }
 
 // The following will be our kernel's entry point.
